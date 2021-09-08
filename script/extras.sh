bff050ee3d7c9b7c2aaaf17658cfa0e10c5ca6b4
Adam Archer <agarcher@gmail.com>
2021-09-08 17:17:51 -0400

    ensure homebrew zsh is in /etc/shells

--- script/extras.sh
+++ script/extras.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# make zsh the default shell
+# make homebrew zsh the default shell
+if ! grep -q "$(which zsh)" /etc/shells; then
+  sudo echo "$(which zsh)" >> /etc/shells
+fi
 chsh -s /usr/local/bin/zsh
 
 exit 0
