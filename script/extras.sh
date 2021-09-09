b90c1f7564cf84229b2d5eca1c10f8c17453ba8e
Adam Archer <agarcher@gmail.com>
2021-09-08 22:48:45 -0400

    suppress output of /etc/shells update

--- script/extras.sh
+++ script/extras.sh
@@ -2,7 +2,8 @@
 
 # make homebrew zsh the default shell
 if ! grep -q "$(which zsh)" /etc/shells; then
-  echo "$(which zsh)" | sudo tee -a /etc/shells
+  echo "adding homebrew zsh to /etc/shells"
+  echo "$(which zsh)" | sudo tee -a /etc/shells >/dev/null
 fi
 chsh -s /usr/local/bin/zsh
 
