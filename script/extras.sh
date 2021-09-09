890ee745120c6005f545ea95a4ed2a70a0ebeac9
Adam Archer <agarcher@gmail.com>
2021-09-08 22:35:44 -0400

    fix flag on tee to /etc/shells

--- script/extras.sh
+++ script/extras.sh
@@ -2,7 +2,7 @@
 
 # make homebrew zsh the default shell
 if ! grep -q "$(which zsh)" /etc/shells; then
-  echo "$(which zsh)" | sudo tee /etc/shells -a
+  echo "$(which zsh)" | sudo tee -a /etc/shells
 fi
 chsh -s /usr/local/bin/zsh
 
