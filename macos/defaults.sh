ec8cc70f52aa3c5086651ceba55f22047bdbd527
Adam Archer <agarcher@gmail.com>
2021-09-10 00:01:10 -0400

    macos, dock, karabiner config

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -33,3 +33,7 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Set dock size settings
+defaults write com.apple.dock tilesize -integer 48
+defaults write com.apple.dock show-recents -bool FALSE
