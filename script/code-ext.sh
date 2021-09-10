5ebe439734ec318f96cfa69ea865332898722fd6
Adam Archer <agarcher@gmail.com>
2021-09-10 09:04:11 -0400

    make code extension script use current directory as working directory

--- script/code-ext.sh
+++ script/code-ext.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")"
+
 # install vscode extensions from codefile
-for EXT in "$(cat script/Codefile)"
+for EXT in "$(cat Codefile)"
 do
   code --install-extension "$EXT"
 done
